8fead10a05beb9671c5f8654984f12a0fa27cdc3
Julian Rich <123442863+ric394@users.noreply.github.com>
2026-04-14 15:09:11 +1000

    Make Supplement classes implement the ISuppInfo interface

--- Models/Grazplan/StoreType.cs
+++ Models/Grazplan/StoreType.cs
@@ -2,6 +2,7 @@
 // GrazPlan Supplement model
 // -----------------------------------------------------------------------
 using System;
+using APSIM.Core;
 using Models.Core;
 
 namespace Models.GrazPlan
@@ -9,25 +10,71 @@ namespace Models.GrazPlan
     /// <summary>
     /// A stored supplement name and quantity
     /// </summary>
+    [ViewName("UserInterface.Views.PropertyView")]
+    [PresenterName("UserInterface.Presenters.PropertyPresenter")]
     [Serializable]
-    public class StoreType : SuppInfo
+    public class StoreType : Model, ISuppInfo
     {
-        /// <summary>
-        /// Gets or sets the name.
-        /// </summary>
-        /// <value>
-        /// The name.
-        /// </value>
-        [Units("-")]
-        public string Name { get; set; }
 
-        /// <summary>
-        /// Gets or sets the amount of supplement.
-        /// </summary>
-        /// <value>
-        /// The description.
-        /// </value>
+        /// <summary>Structure instance supplied by APSIM.core.</summary>
+        [field: NonSerialized]
+        public IStructure Structure { private get; set; }
+        
+        /// <summary>Gets or sets the amount of supplement.</summary>
+        [Description("Amount of supplement (kg)")]
         [Units("kg")]
         public double Stored { get; set; }
+
+        /// <summary>Gets a value indicating whether this is a roughage.</summary>
+        [Description("Is this a roughage?")]
+        public bool IsRoughage { get; set; }
+
+        /// <summary>Gets the dry matter content (0-1).</summary>
+        [Description("Dry matter content (0-1)")]
+        public double DMContent { get; set; }
+
+        /// <summary>Gets the dry matter digestibility (0-1).</summary>
+        [Description("Dry matter digestibility (0-1)")]
+        public double DMD { get; set; }
+
+        /// <summary>Gets the metabolizable energy content (MJ/kg DM).</summary>
+        [Description("Metabolizable energy content (MJ/kg DM)")]
+
+        public double MEContent { get; set; }
+
+        /// <summary>Gets the crude protein concentration (0-1).</summary>
+        [Description("Crude protein concentration (0-1)")]
+        public double CPConc { get; set; }
+
+        /// <summary>Gets the protein degradability (0-1).</summary>
+        [Description("Protein degradability (0-1)")]
+
+        public double ProtDg { get; set; }
+
+        /// <summary>Gets the phosphorus concentration (0-1).</summary>
+        [Description("Phosphorus concentration (0-1)")]
+        public double PConc { get; set; }
+
+        /// <summary>Gets the sulphur concentration (0-1).</summary>
+        [Description("Sulphur concentration (0-1)")]
+        public double SConc { get; set; }
+
+        /// <summary>Gets the ether extract concentration (0-1).</summary>
+        [Description("Ether extract concentration (0-1)")]
+        public double EEConc { get; set; }
+
+        /// <summary>Gets the ADIP to crude protein ratio (0-1).</summary>
+        [Description("ADIP to crude protein ratio (0-1)")]
+        public double ADIP2CP { get; set; }
+
+        /// <summary>Gets the ash alkalinity (mol/kg DM).</summary>
+        [Description("Ash alkalinity (mol/kg DM)")]
+        public double AshAlk { get; set; }
+
+        /// <summary>Gets the maximum passage rate (0-1).</summary>
+        [Description("Maximum passage rate (0-1)")]
+
+        public double MaxPassage { get; set; }
+
     }
 }
--- Models/Grazplan/SuppToStockType.cs
+++ Models/Grazplan/SuppToStockType.cs
@@ -10,7 +10,7 @@ namespace Models.GrazPlan
     /// Paddock and amount of ration
     /// </summary>
     [Serializable]
-    public class SuppToStockType : SuppInfo
+    public class SuppToStockType : Model, ISuppInfo
     {
         /// <summary>
         /// Gets or sets the paddock name.
@@ -35,5 +35,65 @@ namespace Models.GrazPlan
         /// </summary>
         [Units("-")]
         public bool FeedSuppFirst { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether this is a roughage.
+        /// </summary>
+        public bool IsRoughage { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dry matter content.
+        /// </summary>
+        public double DMContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the dry matter digestibility.
+        /// </summary>
+        public double DMD { get; set; }
+
+        /// <summary>
+        /// Gets or sets the metabolizable energy content.
+        /// </summary>
+        public double MEContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets the crude protein concentration.
+        /// </summary>
+        public double CPConc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the protein degradability.
+        /// </summary>
+        public double ProtDg { get; set; }
+
+        /// <summary>
+        /// Gets or sets the phosphorus concentration.
+        /// </summary>
+        public double PConc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the sulphur concentration.
+        /// </summary>
+        public double SConc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ether extract concentration.
+        /// </summary>
+        public double EEConc { get; set; }
+
+        /// <summary>
+        /// Gets or sets the acid detergent insoluble protein to crude protein ratio.
+        /// </summary>
+        public double ADIP2CP { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ash alkalinity.
+        /// </summary>
+        public double AshAlk { get; set; }
+
+        /// <summary>
+        /// Gets or sets the maximum passage rate.
+        /// </summary>
+        public double MaxPassage { get; set; }
     }
 }
