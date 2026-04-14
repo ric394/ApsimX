cf557627a799d0e23e42a056c981a7f8b388f079
Isaiah Huber <ilhuber@iastate.edu>
2026-04-14 11:04:24 -0500

    WL documentation changes.
    Adds a new image for WL documentation, and makes some edits (typos, a
    summary table, references) to the WL documentation class.

--- Models/Soils/Waterlogging.cs
+++ Models/Soils/Waterlogging.cs
@@ -22,10 +22,20 @@ namespace Models.Soils;
 /// however, users can use either SWIM or SoilWat; the new functions work with 
 /// both soil water models.
 ///
+/// ## Summary table of processes affected by waterlogging
+///
+/// |Process|Driver|Model Approach| Crop-specific?|
+/// |-------|------|--------------|---------------|
+/// |Root growth| AFPS| XY Modifier on root front velocity|Yes|
+/// |RUE| Wet root fraction|Min(stress today, stress from legacy effect)|Crop and Stage Specific|
+/// |Phenology|Cumulative Excess Water Stress|Phyllochron Modifier|Optional|
+/// |Senesence|Cumulative Excess Water Stress|Leaf model modifier|Yes|
+/// |Grain components|Cumulative Excess Water Stress| Penalty Functions| Yes|
+///
 /// ## Roots
 ///
 /// We incorporated into the model the approach we already had in APSIM Classic 
-/// (Ebrahimi-Mollabashi et al., 2019; Archontoulis et al., 2020). 
+/// (Ebrahimi-Mollabashi et al., 2019; Archontoulis et al., 2020, Liu et al. 2021, 2023).
 /// Excess moisture affects the root front.  More specifically, when the 
 /// air-filled pore space exceeds 97% of saturation, the root front velocity 
 /// decreases for the period of excess stress. Model simulations showed 
@@ -35,11 +45,11 @@ namespace Models.Soils;
 ///
 /// ![Soybean root depth](waterlogging-soybean-root-depth.png)
 ///
-/// ## Radiation Use Efficiently
+/// ## Radiation Use Efficiency
 ///
 /// Excess moisture stress affects RUE like drought stress. Hence, we updated 
-/// the model to calculate water stress on RUE by considering both “Deficit” 
-/// and “Excess” moisture stress via the “Minimum Function” (see tree). 
+/// the model to calculate water stress on RUE by considering both “Deficient”
+/// and “Excess” moisture stress via the “Minimum Function” (i.e. RUE FW = min(F_deficient, F_excess), see model structure).
 /// The driver for excess moisture stress is the wet root fraction, which is 
 /// calculated as (sw-dul)/(sat-dul)). A 0 to 1 daily value is computed as the 
 /// average of it weighted by root length density. Then we use this 
@@ -75,7 +85,7 @@ namespace Models.Soils;
 ///
 /// ## Crop Phenology
 ///
-/// Excess moisture could delay phenology. We model this phenomenon by 
+/// Excess moisture could delay phenology (Liu et al., 2020). We model this phenomenon by
 /// adjusting the phyllochron parameter via an XY modifier. In the model, the 
 /// driver for this delay (x-axis) is the cumulative excess water stress 
 /// (“CumulativeExcessWaterStress”). This is off by default but remains 
@@ -107,6 +117,8 @@ namespace Models.Soils;
 /// cumulative excess water stress days increase beyond 5.  Please see 
 /// model structure for the XY modifiers.
 ///
+/// ![Gain components](waterlogging-grain-components.png)
+///
 /// ## Sensibility test
 ///
 /// We run three soybean simulations reflecting 3 weather 
@@ -133,11 +145,19 @@ namespace Models.Soils;
 ///
 /// ## References
 ///
-/// Ebrahimi Mollaboashi et al. 2019
+/// Ebrahimi-Mollabashi E, Huth NI, Holzwoth DP, Ordonez RS, Hatfield JL, Huber I, Castellano MJ, Archontoulis SV, 2019. Enhancing APSIM to simulate excessive moisture effects on root growth. Field Crops Research 236: 58–67.
 ///
-/// Pasley et al., 2020
+/// Pasley HR, Huber I, Castellano MJ, Archontoulis SV, 2020. Modeling flood-induced stress in soybeans. Frontiers Plant Science 11:62, doi:10.3389/fpls.2020.00062.
 ///
-/// Ke/Matt paper 1. 2, 3, 4, 5
+/// Archontoulis SV, Castellano MJ, Licht MA, Nichols V, Baum M, Huber I, Martinez-Feria R, Puntel L, Ordónez RA, Iqbal J, Wright EE, Dietzel RN, Helmers M, Vanloocke A, Liebman M, Hatfield JL, Herzmann D, Cordova SC, Edmonds P, Togliatti K, Kessler A, Danalatos G, Pasley H, Pederson C, Lamkey KR, 2020. Predicting Crop Yields and Soil-Plant Nitrogen Dynamics in the US Corn Belt. Crop Science, 60: 721–738.
+///
+/// Garcia-Vila M, M dos Santos Vianna, MT Harrison, K Liu, R de S. Nóia-Júnior, T Weber, J Zhao, M Acutis, SV Archontoulis, S Asseng, P Aubry, J Balkovic, B Basso, X Chen, Y Chen, Q de Jong van Lier, M Delandmeter, A de Wit, B Dumont, R Ferrise, C Folberth, M Gabbrielli, T Gaiser, A Gorooei, G Hoogenboom, KC Kersebaum, YU Kim, D Kraus, B Liu, L Martin, K Metselaar, C Nendel, G Padovan, A Perego, DM Seserman, C Scheer, V Shelia, V Stocca, F Tao, E Wang, H Webber, Z Zhao, Y Zhu, T Palosuo (2025) Gaps and strategies for accurate simulation of waterlogging impacts on crop productivity. Nat Food, 6: 553-562. doi:10.1038/s43016-025-01179-y
+///
+/// Liu K, Harrison MT, Yan H, Liu DL, Meinke H, Hoogenboom G, Wang B, Peng B, Guan K, Jaegermeyr J, Wang E, Zhang F, Yin X, Archontoulis S, Nie L, Badea A, Man J, Wallach D, Zhao J, Benjumea AB, Fahad S, Tian X, Wang W, Tao F, Zhang Z, Rötter R, Yuan Y, Zhu M, Dai P, Nie J, Yang Y, Zhang Y, Zhou M, 2023. Silver lining to a climate crisis in multiple prospects for alleviating crop waterlogging under future climates. Nature Communications 14, 765.
+///
+/// Liu, K., Harrison, M. T., Archontoulis, S. V., Huth, N., Yang, R., Liu, D. L., Yan, H.L., Meinke, H., Huber, I., Ibrahim, A., Zhang, Y.B. Tian, X.H &amp; Zhou, M. (2021). Climate change shifts forward flowering and reduces crop waterlogging stress. Environmental Research Letters, 16(9), 094017
+///
+/// Liu, K., Harrison, M. T., Ibrahim, A., Manik, S. N., Johnson, P., Tian, X., Meinke, H.,  Zhou, M. (2020). Genetic factors increasing barley grain yields under soil waterlogging. Food and Energy Security, 9(4), e238
 /// </summary>
 [ValidParent(ParentType = typeof(Plant))]
 public class Waterlogging : Model
