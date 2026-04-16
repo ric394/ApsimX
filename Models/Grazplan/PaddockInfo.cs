43d738b6e625dd50ec8539d18b482285ec25186e
Julian Rich <123442863+ric394@users.noreply.github.com>
2026-04-16 13:56:14 +1000

    Put PaddockInfo back to as part of SupplementModel

--- Models/Grazplan/PaddockInfo.cs
+++ Models/Grazplan/PaddockInfo.cs
@@ -5,32 +5,33 @@ using System;
 
 namespace Models.GrazPlan
 {
-
-    /// <summary>
-    /// Paddock information about the supplement fed
-    /// </summary>
-    [Serializable]
-    private class PaddockInfo
+    public partial class SupplementModel
     {
         /// <summary>
-        /// The name
+        /// Paddock information about the supplement fed
         /// </summary>
-        public string Name;
+        [Serializable]
+        private class PaddockInfo
+        {
+            /// <summary>
+            /// The name
+            /// </summary>
+            public string Name;
 
-        /// <summary>
-        /// The padd identifier
-        /// </summary>
-        public int PaddId;
+            /// <summary>
+            /// The padd identifier
+            /// </summary>
+            public int PaddId;
 
-        /// <summary>
-        /// The supplement fed
-        /// </summary>
-        public SupplementRation SupptFed;   // Entry N is the supplement fed out N days ago
+            /// <summary>
+            /// The suppt fed
+            /// </summary>
+            public SupplementRation SupptFed;   // Entry N is the supplement fed out N days ago
 
-        /// <summary>
-        /// For bail feeding
-        /// </summary>
-        public bool FeedSuppFirst = false;
+            /// <summary>
+            /// For bail feeding
+            /// </summary>
+            public bool FeedSuppFirst = false;
+        }
     }
-    
 }
